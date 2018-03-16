693667baea095a4c0bd903f91c5f1580996ff0c0
Lawrence Thatcher <lwthatcher@msn.com>
2018-03-16 17:08:41 -0600

    wrapped onselect to handle different sides

--- lasso2.py
+++ lasso2.py
@@ -15,7 +15,7 @@ ax.imshow(img)
 
 ax2 = fig.add_subplot(122)
 array = np.zeros(img.shape[:-1])
-msk = ax2.imshow(array, origin='upper', vmax=1, interpolation='nearest')
+msk = ax2.imshow(array, origin='upper', vmin=-10, vmax=10, interpolation='nearest')
 print("Image shape", img.shape, array.shape)
 
 xv, yv = np.meshgrid(np.arange(img.shape[1]), np.arange(img.shape[0]))
@@ -26,37 +26,43 @@ lpl = dict(color='blue', linestyle='-', linewidth=5, alpha=0.5)
 lpr = dict(color='black', linestyle='-', linewidth=5, alpha=0.5)
 
 
-def updateArray(array, indices):
+def updateArray(array, indices, val):
     lin = np.arange(array.size)
     newArray = array.flatten()
-    newArray[lin[indices]] = 1
+    newArray[lin[indices]] = val
     return newArray.reshape(array.shape)
 
 
-def onselect(verts):
-    p = path.Path(verts)
-    ind = p.contains_points(idx, radius=5)
-    v0 = np.array(verts)
-    v1 = np.round(verts)
-    v2 = np.unique(v1, axis=0)
-    # selections
-    print(v0.shape, v1.shape, v2.shape, idx[ind].shape)
-    global array
-    array = updateArray(array, ind)
-    msk.set_data(array)
-    fig.canvas.draw_idle()
+def select_callback(side='left'):
+    val = 10
+    if side == 'right':
+        val = -10
 
-    # draw the line again, just for fun!
-    # line = Line2D(v0[:,0], v0[:,1], **lpl)
-    # ax2.add_line(line)
-    # ax2.draw_artist(line)
-    # line.set_visible(True)
-    # fig.canvas.draw_idle()
-    # print(line, line.get_data()[0].shape)
+    def onselect(verts):
+        p = path.Path(verts)
+        ind = p.contains_points(idx, radius=5)
+        v0 = np.array(verts)
+        v1 = np.round(verts)
+        v2 = np.unique(v1, axis=0)
+        print('side:', side)
+        # selections
+        print(v0.shape, v1.shape, v2.shape, idx[ind].shape)
+        global array
+        array = updateArray(array, ind, val)
+        msk.set_data(array)
+        fig.canvas.draw_idle()
+        # draw the line again, just for fun!
+        # line = Line2D(v0[:,0], v0[:,1], **lpl)
+        # ax2.add_line(line)
+        # ax2.draw_artist(line)
+        # line.set_visible(True)
+        # fig.canvas.draw_idle()
+        # print(line, line.get_data()[0].shape)
+    return onselect
 
 
-lasso_left = LassoSelector(ax, onselect, lineprops=lpl, button=[1])
-lasso_right = LassoSelector(ax, onselect, lineprops=lpr, button=[3])
+lasso_left = LassoSelector(ax, select_callback('left'), lineprops=lpl, button=[1])
+lasso_right = LassoSelector(ax, select_callback('right'), lineprops=lpr, button=[3])
 
 plt.show()
 
