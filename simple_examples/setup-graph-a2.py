30ccd7ec7f7621c7c6244f9c70fc4ebd9d8c76e1
Lawrence Thatcher <lwthatcher@msn.com>
2018-04-17 00:22:01 -0600

    almost getting F/G weights

--- simple_examples/setup-graph-a2.py
+++ simple_examples/setup-graph-a2.py
@@ -6,10 +6,13 @@ from matplotlib import pyplot as plt
 
 a = imread("../img/a.png")
 img = imread("../img/a2.png")
+print(img.shape)
 
-print(a.shape)
+# variance
+σ2 = np.var(img)
 
 
+# region Helper Functions
 def circle_mask(_img, origin, r):
     a, b = origin
     n0, n1 = _img.shape
@@ -39,29 +42,66 @@ def a2_markings(plot=True):
     return markings
 
 
-seeds = a2_markings()
+def av_dist(_img, mask):
+    result = np.empty(_img.shape)
+    for index, x in np.ndenumerate(_img):
+        result[index] = np.mean(_dist(x, _img[mask]))
+    return result
 
-# # Create the graph.
-# g = maxflow.Graph[int]()
-# # Add the nodes. nodeids has the identifiers of the nodes in the grid.
-# nodeids = g.add_grid_nodes(img.shape)
-# # Add non-terminal edges with the same capacity.
-# g.add_grid_edges(nodeids, 50)
-# # Add the terminal edges. The image pixels are the capacities
-# # of the edges from the source node. The inverted image pixels
-# # are the capacities of the edges to the sink node.
-# g.add_grid_tedges(nodeids, img, 255-img)
+
+def _dist(a,b):
+    return np.around(1 - np.exp(-(((a-b)**2)/(2*σ2))), 4)
+# endregion
+
+
+# get seeds
+seeds = a2_markings(plot=False)
+S = (255 - seeds[..., 0]) / 255
+T = (255 - seeds[..., 2]) / 255
+print('S', np.unique(S, return_counts=True))
+print('T', np.unique(T, return_counts=True))
+# s/t masks
+s_mask = S == 1
+t_mask = T == 1
+print('S mask')
+print(img[s_mask])
+print('T mask')
+print(img[t_mask])
+
+# define foreground/background
+F = av_dist(img, s_mask)
+B = av_dist(img, t_mask)
+
+plt.imshow(F,  cmap=plt.cm.gray)
+plt.show()
+
+plt.imshow(B,  cmap=plt.cm.gray)
+plt.show()
+
+# infinite cost for marked spots
+F[s_mask] = np.inf
+B[t_mask] = np.inf
+
+print(F)
+
+# Create the graph.
+g = maxflow.Graph[int]()
+# Add the nodes. nodeids has the identifiers of the nodes in the grid.
+nodeids = g.add_grid_nodes(img.shape)
+# Add non-terminal edges with the same capacity.
+g.add_grid_edges(nodeids, 1)
+
+g.add_grid_tedges(nodeids, F, B)
 #
 #
-# # Find the maximum flow.
-# g.maxflow()
-# # Get the segments of the nodes in the grid.
-# sgm = g.get_grid_segments(nodeids)
-#
-#
-# # The labels should be 1 where sgm is False and 0 otherwise.
-# img2 = np.int_(np.logical_not(sgm))
-# # Show the result.
-#
-# plt.imshow(img2, cmap=plt.cm.gray, interpolation='nearest')
-# plt.show()
+# Find the maximum flow.
+g.maxflow()
+# Get the segments of the nodes in the grid.
+sgm = g.get_grid_segments(nodeids)
+
+# The labels should be 1 where sgm is False and 0 otherwise.
+img2 = np.int_(np.logical_not(sgm))
+# Show the result.
+
+plt.imshow(img2, cmap=plt.cm.gray, interpolation='nearest')
+plt.show()
