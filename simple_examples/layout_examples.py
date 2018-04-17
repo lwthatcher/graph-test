90c87f3fc1d3efbcd2b2bb7b5eed6cbcb91f3203
Lawrence Thatcher <lwthatcher@msn.com>
2018-04-16 20:11:05 -0600

    extracted various graphs to functions

--- simple_examples/layout_examples.py
+++ simple_examples/layout_examples.py
@@ -4,6 +4,7 @@ This file contains a list of examples with different layouts that can be
 obtained using the ``add_grid_edges`` method.
 """
 
+import argparse
 import numpy as np
 import maxflow
 import networkx as nx
@@ -39,60 +40,85 @@ def plot_graph_2d(graph, nodes_shape, plot_weights=True, plot_terminals=True, fo
         plt.suptitle(title)
     plt.show()
 
-# Standard 4-connected grid
-g = maxflow.Graph[int]()
-nodeids = g.add_grid_nodes((5, 5))
-g.add_grid_edges(nodeids, 1)
-# Equivalent to
-# structure = maxflow.vonNeumann_structure(ndim=2, directed=False)
-# g.add_grid_edges(nodeids, 1,
-#                  structure=structure,
-#                  symmetric=False)
-plot_graph_2d(g, nodeids.shape, plot_terminals=False, title='Standard 4-connected grid')
+def A():
+    # Standard 4-connected grid
+    g = maxflow.Graph[int]()
+    nodeids = g.add_grid_nodes((5, 5))
+    g.add_grid_edges(nodeids, 1)
+    # Equivalent to
+    # structure = maxflow.vonNeumann_structure(ndim=2, directed=False)
+    # g.add_grid_edges(nodeids, 1,
+    #                  structure=structure,
+    #                  symmetric=False)
+    plot_graph_2d(g, nodeids.shape, plot_terminals=False, title='Standard 4-connected grid')
 
-# 8-connected grid
-g = maxflow.Graph[int]()
-nodeids = g.add_grid_nodes((5, 5))
-structure = np.array([[0, 0, 0],
-                      [0, 0, 1],
-                      [1, 1, 1]])
-# Also structure = maxflow.moore_structure(ndim=2, directed=True)
-g.add_grid_edges(nodeids, 1, structure=structure, symmetric=True)
-plot_graph_2d(g, nodeids.shape, plot_terminals=False, title='8-connected grid')
 
-# 24-connected 5x5 neighborhood
-g = maxflow.Graph[int]()
-nodeids = g.add_grid_nodes((5, 5))
-structure = np.array([[1, 1, 1, 1, 1],
-                      [1, 1, 1, 1, 1],
-                      [1, 1, 0, 1, 1],
-                      [1, 1, 1, 1, 1],
-                      [1, 1, 1, 1, 1]])
-g.add_grid_edges(nodeids, 1, structure=structure, symmetric=False)
-plot_graph_2d(g, nodeids.shape, plot_terminals=False, plot_weights=False, title='24-connected 5x5 neighborhood')
+def B():
+    # 8-connected grid
+    g = maxflow.Graph[int]()
+    nodeids = g.add_grid_nodes((5, 5))
+    structure = np.array([[0, 0, 0],
+                          [0, 0, 1],
+                          [1, 1, 1]])
+    # Also structure = maxflow.moore_structure(ndim=2, directed=True)
+    g.add_grid_edges(nodeids, 1, structure=structure, symmetric=True)
+    plot_graph_2d(g, nodeids.shape, plot_terminals=False, title='8-connected grid')
 
-# Diagonal, not symmetric
-g = maxflow.Graph[int]()
-nodeids = g.add_grid_nodes((5, 5))
-structure = np.array([[0, 0, 0],
-                      [0, 0, 0],
-                      [0, 0, 1]])
-g.add_grid_edges(nodeids, 1, structure=structure, symmetric=False)
-plot_graph_2d(g, nodeids.shape, plot_terminals=False, title='Diagonal, not symmetric')
 
-# Central node connected to every other node
-g = maxflow.Graph[int]()
-nodeids = g.add_grid_nodes((5, 5)).ravel()
+def C():
+    # 24-connected 5x5 neighborhood
+    g = maxflow.Graph[int]()
+    nodeids = g.add_grid_nodes((5, 5))
+    structure = np.array([[1, 1, 1, 1, 1],
+                          [1, 1, 1, 1, 1],
+                          [1, 1, 0, 1, 1],
+                          [1, 1, 1, 1, 1],
+                          [1, 1, 1, 1, 1]])
+    g.add_grid_edges(nodeids, 1, structure=structure, symmetric=False)
+    plot_graph_2d(g, nodeids.shape, plot_terminals=False, plot_weights=False, title='24-connected 5x5 neighborhood')
 
-central_node = nodeids[12]
-rest_of_nodes = np.hstack([nodeids[:12], nodeids[13:]])
 
-nodeids = np.empty((2, 24), dtype=np.int_)
-nodeids[0] = central_node
-nodeids[1] = rest_of_nodes
+def D():
+    # Diagonal, not symmetric
+    g = maxflow.Graph[int]()
+    nodeids = g.add_grid_nodes((5, 5))
+    structure = np.array([[0, 0, 0],
+                          [0, 0, 0],
+                          [0, 0, 1]])
+    g.add_grid_edges(nodeids, 1, structure=structure, symmetric=False)
+    plot_graph_2d(g, nodeids.shape, plot_terminals=False, title='Diagonal, not symmetric')
 
-structure = np.array([[0, 0, 0],
-                      [0, 0, 0],
-                      [0, 1, 0]])
-g.add_grid_edges(nodeids, 1, structure=structure, symmetric=False)
-plot_graph_2d(g, (5, 5), plot_terminals=False, title='Central node connected to every other node')
+
+def E():
+    # Central node connected to every other node
+    g = maxflow.Graph[int]()
+    nodeids = g.add_grid_nodes((5, 5)).ravel()
+
+    central_node = nodeids[12]
+    rest_of_nodes = np.hstack([nodeids[:12], nodeids[13:]])
+
+    nodeids = np.empty((2, 24), dtype=np.int_)
+    nodeids[0] = central_node
+    nodeids[1] = rest_of_nodes
+
+    structure = np.array([[0, 0, 0],
+                          [0, 0, 0],
+                          [0, 1, 0]])
+    g.add_grid_edges(nodeids, 1, structure=structure, symmetric=False)
+    plot_graph_2d(g, (5, 5), plot_terminals=False, title='Central node connected to every other node')
+
+
+funcs = {'A': A,
+         'B': B,
+         'C': C,
+         'D': D,
+         'E': E}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('plots', nargs='+', choices=['A', 'B', 'C', 'D', 'E'], help='list of plots you want to show')
+    args = parser.parse_args()
+    for plot in args.plots:
+        print('PLOT {}'.format(plot))
+        funcs[plot]()
