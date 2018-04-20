8c949384475a91482edb46659a661594929414fc
Lawrence Thatcher <lwthatcher@msn.com>
2018-04-19 22:12:25 -0600

    rectangle and lasso mode

--- dev_examples/rectangle-seeds.py
+++ dev_examples/rectangle-seeds.py
@@ -8,16 +8,24 @@ method 'self.ignore()' it is checked whether the button from eventpress
 and eventrelease are the same.
 
 """
-from matplotlib.widgets import RectangleSelector
+from matplotlib.widgets import RectangleSelector, LassoSelector
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button, RadioButtons
+import matplotlib.patches as patches
+from matplotlib.widgets import RadioButtons
 from PIL import Image
+from matplotlib import path
 
 img = np.asarray(Image.open("../img/gymnastics.jpg"))
-msk = np.zeros(img.shape[:-1])
+msk = np.zeros(img.shape)
 
-# fig = plt.figure(figsize=(24, 16))
+
+xv, yv = np.meshgrid(np.arange(img.shape[1]), np.arange(img.shape[0]))
+idx = np.vstack((xv.flatten(), yv.flatten())).T
+
+print('IDX', idx.shape)
+
+fig = plt.figure(figsize=(24, 16))
 axcolor = 'lightgoldenrodyellow'
 ax1 = plt.subplot2grid((3, 5), (0, 0), facecolor=axcolor)
 ax2 = plt.subplot2grid((3, 5), (0, 1), rowspan=3, colspan=2)
@@ -26,11 +34,10 @@ ax3 = plt.subplot2grid((3, 5), (0, 3), rowspan=3, colspan=2)
 ax2.set_xticks([]), ax2.set_yticks([])
 ax3.set_xticks([]), ax3.set_yticks([])
 
-# axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-# axamp = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
-#
-# sfreq = Slider(axfreq, 'Freq', 0.1, 30.0, valinit=f0)
-# samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0)
+_img = ax2.imshow(img)
+_msk = ax3.imshow(msk, origin='upper', interpolation='nearest')
+
+rect = None
 
 
 def line_select_callback(eclick, erelease):
@@ -39,41 +46,81 @@ def line_select_callback(eclick, erelease):
     x2, y2 = erelease.xdata, erelease.ydata
     print("(%3.2f, %3.2f) --> (%3.2f, %3.2f)" % (x1, y1, x2, y2))
     print(" The button you used were: %s %s" % (eclick.button, erelease.button))
+    global rect
+    x,y = min(x1,x2), min(y1,y2)
+    width, height = max(x1,x2)-x, max(y1,y2)-y
+    rect = patches.Rectangle((x,y), width, height, color='blue', visible=True)
+    print('RECT:', rect)
+    # add rectangle
+    ax3.add_patch(rect)
+    fig.canvas.draw_idle()
 
 
 def toggle_selector(event):
-    print(' Key pressed.')
-    if event.key in ['Q', 'q'] and toggle_selector.RS.active:
-        print(' RectangleSelector deactivated.')
-        toggle_selector.RS.set_active(False)
-    if event.key in ['A', 'a'] and not toggle_selector.RS.active:
-        print(' RectangleSelector activated.')
+    print(' Key pressed.', event.key)
+    global rect
+    if event.key == 'escape' and rect:
+        print('CLEAR rect!', rect)
+        rect.remove()
+        rect = None
+        fig.canvas.draw_idle()
+
+
+def radio_callback(label):
+    print('selected', label)
+    if label == 'rectangle':
         toggle_selector.RS.set_active(True)
+        toggle_selector.LL.set_active(False)
+        toggle_selector.LR.set_active(False)
+    elif label == 'lasso':
+        toggle_selector.RS.set_active(False)
+        toggle_selector.LL.set_active(True)
+        toggle_selector.LR.set_active(True)
+    elif label == 'draw':
+        toggle_selector.RS.set_active(False)
+        toggle_selector.LL.set_active(False)
+        toggle_selector.LR.set_active(False)
 
 
-# fig, current_ax = plt.subplots()                 # make a new plotting range
-# N = 100000                                       # If N is large one can see
-# x = np.linspace(0.0, 10.0, N)                    # improvement by use blitting!
+def _update_array(ind, dim):
+    """draws the red or blue seeds on a white background"""
+    global msk
+    channels = np.arange(3)
+    a,b = ind.T
+    xi = channels[channels != dim]
+    we = np.meshgrid(a,xi)[1]
+    msk[b, a, we] = 0
+    return msk
 
-# ax2.plot(x, +np.sin(.2*np.pi*x), lw=3.5, c='b', alpha=.7)  # plot something
-# ax2.plot(x, +np.cos(.2*np.pi*x), lw=3.5, c='r', alpha=.5)
-# ax3.plot(x, -np.sin(.2*np.pi*x), lw=3.5, c='g', alpha=.3)
 
-# draw image
-ax2.imshow(img)
-# draw selection mask
-ax3.imshow(msk)
+def lasso_callback(dim):
+    def onselect(verts):
+        p = path.Path(verts)
+        print('p', len(verts))
+        ind = p.contains_points(idx, radius=5)
+        global msk, _msk
+        msk = _update_array(idx[ind], dim)
+        _msk.set_data(msk)
+        fig.canvas.draw_idle()
+    return onselect
 
-# rax = plt.axes([0.025, 0.5, 0.15, 0.15], facecolor=axcolor)
-radio = RadioButtons(ax1, ('red', 'blue', 'green'), active=0)
+
+lpl = dict(color='blue', linestyle='-', linewidth=5, alpha=0.5)
+lpr = dict(color='black', linestyle='-', linewidth=5, alpha=0.5)
 
 # drawtype is 'box' or 'line' or 'none'
 toggle_selector.RS = RectangleSelector(ax2, line_select_callback,
                                        drawtype='box', useblit=True,
-                                       button=[1, 3],  # don't use middle button
-                                       minspanx=5, minspany=5,
-                                       spancoords='pixels',
-                                       interactive=True)
+                                       button=[1], minspanx=5, minspany=5,
+                                       spancoords='data', interactive=True)
+toggle_selector.LL = LassoSelector(ax2, lasso_callback(2), lineprops=lpl, button=[1])
+toggle_selector.LR = LassoSelector(ax2, lasso_callback(0), lineprops=lpr, button=[3])
+toggle_selector.LL.set_active(False)
+toggle_selector.LR.set_active(False)
+
+# rax = plt.axes([0.025, 0.5, 0.15, 0.15], facecolor=axcolor)
+radio = RadioButtons(ax1, ('rectangle', 'lasso', 'draw'), active=0)
+radio.on_clicked(radio_callback)
 
 plt.tight_layout()
 plt.connect('key_press_event', toggle_selector)
