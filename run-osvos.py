1700bd508f3a60ff8264e71c6ffd58b97fb7f53f
Lawrence Thatcher <lwthatcher@msn.com>
2018-04-24 19:56:12 -0600

    updated to work in current structure

--- run-osvos.py
+++ run-osvos.py
@@ -24,18 +24,16 @@ from osvos.dataset import Dataset
 os.chdir(root_folder)
 
 # define acceptable sequences
-_path = os.path.join('DAVIS', 'JPEGImages', '480p')
+_path = os.path.join('img', 'DAVIS', 'images')
 known_sequences = next(os.walk(_path))[1]
 
 
-def run_demo(seq_name, max_training_iters=500, **kwargs):
+def run_demo(seq_name, max_training_iters=200, **kwargs):
     # User Defined parameters
-    gpu_id = kwargs.get('gpu_id', 0)
-    train_model = kwargs.get('train_model', True)
     side_supervision = kwargs.get('side_supervision', 1)
 
     # define paths
-    result_path = os.path.join('DAVIS', 'Results', 'Segmentations', '480p', 'OSVOS', seq_name)
+    result_path = os.path.join('img', 'DAVIS', 'results', seq_name)
     parent_path = os.path.join('models', 'OSVOS_parent', 'OSVOS_parent.ckpt-50000')
     logs_path = os.path.join('models', seq_name)
 
@@ -45,28 +43,22 @@ def run_demo(seq_name, max_training_iters=500, **kwargs):
     display_step = kwargs.get('display_step', 10)
 
     # Define Dataset
-    test_frames = sorted(os.listdir(os.path.join('DAVIS', 'JPEGImages', '480p', seq_name)))
-    test_imgs = [os.path.join('DAVIS', 'JPEGImages', '480p', seq_name, frame) for frame in test_frames]
-    if train_model:
-        train_imgs = [os.path.join('DAVIS', 'JPEGImages', '480p', seq_name, '00000.jpg')+' '+
-                      os.path.join('DAVIS', 'Annotations', '480p', seq_name, '00000.png')]
-        dataset = Dataset(train_imgs, test_imgs, './', data_aug=True)
-    else:
-        dataset = Dataset(None, test_imgs, './')
+    test_frames = sorted(os.listdir(os.path.join('img', 'DAVIS', 'images', seq_name)))
+    test_imgs = [os.path.join('img', 'DAVIS', 'images', seq_name, frame) for frame in test_frames]
+    train_frames = sorted(os.listdir(os.path.join('img', 'DAVIS', 'train', seq_name)))
+    train_imgs = [os.path.join('img', 'DAVIS', 'train', seq_name, frame) for frame in train_frames]
+    dataset = Dataset(train_imgs, test_imgs, './', data_aug=True)
 
     # Train the network
-    if train_model:
-        with tf.Graph().as_default():
-            with tf.device('/gpu:' + str(gpu_id)):
-                global_step = tf.Variable(0, name='global_step', trainable=False)
-                osvos.train_finetune(dataset, parent_path, side_supervision, learning_rate, logs_path, max_training_iters,
-                                     save_step, global_step, iter_mean_grad=1, ckpt_name=seq_name)
+    with tf.Graph().as_default():
+        global_step = tf.Variable(0, name='global_step', trainable=False)
+        osvos.train_finetune(dataset, parent_path, side_supervision, learning_rate, logs_path, max_training_iters,
+                             save_step, global_step, iter_mean_grad=1, ckpt_name=seq_name)
 
     # Test the network
     with tf.Graph().as_default():
-        with tf.device('/gpu:' + str(gpu_id)):
-            checkpoint_path = os.path.join('models', seq_name, seq_name+'.ckpt-'+str(max_training_iters))
-            osvos.test(dataset, checkpoint_path, result_path)
+        checkpoint_path = os.path.join('models', seq_name, seq_name+'.ckpt-'+str(max_training_iters))
+        osvos.test(dataset, checkpoint_path, result_path)
 
 
 def _sanitize_kwargs(kwargs):
@@ -83,9 +75,8 @@ if __name__ == "__main__":
     # define parser
     parser = argparse.ArgumentParser('parameters for running OSVOS')
     parser.add_argument('seq_name', choices=known_sequences, help='the name of the sequence to run')
-    parser.add_argument('--epochs', '-t', dest='max_training_iters', default=500, type=int,
+    parser.add_argument('--epochs', '-t', dest='max_training_iters', default=200, type=int,
                         help='number of epochs to train for')
-    parser.add_argument('--gpu-id', type=int, help='the id if the GPU to use')
     parser.add_argument('--supervision', dest='side_supervision', choices=[1, 2, 3], type=int,
                         help='Level of the side outputs supervision: 1-Strong 2-Weak 3-No supervision')
     parser.add_argument('--learning-rate', '-lr', type=float, help='the base learning rate')
